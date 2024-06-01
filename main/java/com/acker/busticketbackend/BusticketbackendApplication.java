e037f4ca77b37ddd2f7582d1de88a19c027311ef
Amirthanathan R <amirth300324@gmail.com>
2024-06-01 19:02:00 +0530

    Initializing Spring Project

---
+++ main/java/com/acker/busticketbackend/BusticketbackendApplication.java
@@ -0,0 +1,13 @@
+package com.acker.busticketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketbackendApplication.class, args);
+	}
+
+}
