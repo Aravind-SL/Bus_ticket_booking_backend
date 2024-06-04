94d833948c46186171e967c5e558e338a9888aac
aravind <aravind9180@gmail.com>
2024-06-04 18:19:13 +0530

    Added Seats Logic

--- src/main/java/com/acker/busticketbackend/routes/Routes.java
+++ src/main/java/com/acker/busticketbackend/routes/Routes.java
@@ -1,18 +1,10 @@
 package com.acker.busticketbackend.routes;
 
-import java.util.List;
-
-import com.acker.busticketbackend.station.Station;
-import com.acker.busticketbackend.buses.Bus;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- src/main/java/com/acker/busticketbackend/station/Station.java
+++ src/main/java/com/acker/busticketbackend/station/Station.java
@@ -1,20 +1,15 @@
 package com.acker.busticketbackend.station;
 
-import com.acker.busticketbackend.routes.Routes;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List; 
+import lombok.NoArgsConstructor; 
 
 @Entity
 @Data
