cd1dfb8352e181ece1ea2ba559d94e227608c8da
Amirthanathan R <amirth300324@gmail.com>
2024-06-11 14:43:44 +0530

    Added Basic User service. #20

---
+++ src/main/java/com/acker/busticketbackend/users/UserListItem.java
@@ -0,0 +1,20 @@
+package com.acker.busticketbackend.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserListItem {
+    private Integer id;
+    private String username;
+
+
+    private String firstName, lastName;
+
+
+}
